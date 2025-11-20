ae476e8997b51b497fe39308ab232edc65f92b7a
EX1MA <joelc309@gmail.com>
2025-11-20 00:03:13 -0600

    Configurando despliegue

--- Portafolio/vite.config.ts
+++ Portafolio/vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANTE: Cambia esto por el nombre de tu repositorio entre barras
+  // Ejemplo: base: '/mi-portafolio/',
+  base: '/silverinvention.github.io/', 
+  
   optimizeDeps: {
-    // Esto obliga a Vite a preparar esta librería explícitamente
     include: ['react/jsx-runtime'],
   },
 })
